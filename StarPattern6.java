6e500f6b83527b3c3de9283aabd73b5baadd8fda
Aditya Satyawan Waghmare <142006839+adityasw65@users.noreply.github.com>
2024-11-15 09:52:32 +0530

    Add files via upload

---
+++ StarPattern6.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class StarPattern6 {
+   public static void main(String[] args) {
+
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter your row (odd number) : ");
+      int row = sc.nextInt();
+      for(int i = 0; i < row; i++) {
+         for(int j = 0; j < row; j++) {
+            if(i <= (row / 2) && j > (row / 2) - i && j < (row / 2) + i)
+               System.out.print(" ");
+            else if(i > (row / 2) && j > i - (row / 2) && j < ((row - i) + (row / 2)))
+               System.out.print(" ");
+            else
+               System.out.print("*");
+            System.out.print(" ");
+         }
+         System.out.println();
+      }
+
+   }
+}
